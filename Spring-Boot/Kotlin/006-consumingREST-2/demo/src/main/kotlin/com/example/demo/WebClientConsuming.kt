273e50270d9483f0ab792de2ade3da828f8d6aa4
reruo321 <48712088+reruo321@users.noreply.github.com>
2022-04-19 00:30:20 +0900

    Update WebClientConsuming.kt

--- Spring-Boot/Kotlin/006-consumingREST-2/demo/src/main/kotlin/com/example/demo/WebClientConsuming.kt
+++ Spring-Boot/Kotlin/006-consumingREST-2/demo/src/main/kotlin/com/example/demo/WebClientConsuming.kt
@@ -1,13 +1,27 @@
 package com.example.demo
 
 import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootApplication
 class WebClientConsuming{
     companion object{
-        private val log = LoggerFactory.getLogger(WebClientConsuming::class.java)
+	private val log = LoggerFactory.getLogger(WebClientConsuming::class.java)
+    }
+    @Bean
+    fun webClientRestCall(): CommandLineRunner {
+	return CommandLineRunner {
+	    val client = WebClient.builder().build()
+	    val commentFlux = client.get()
+		    .uri("https://jsonplaceholder.typicode.com/comments")
+		    .retrieve()
+		    .bodyToFlux(Comment::class.java)
+	    commentFlux.subscribe(System.out::println)
+	}
     }
 }
 
