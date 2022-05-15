ffb1cc03b638f4c942b373d97d3bc6c1b5537487
reruo <reruo321@gmail.com>
2022-05-16 01:23:09 +0900

    updating test

--- Spring-Boot/Kotlin/007-Spring-Boot-with-MongoDB/src/test/kotlin/com/example/demo/WebClientConsumingTest.kt
+++ Spring-Boot/Kotlin/007-Spring-Boot-with-MongoDB/src/test/kotlin/com/example/demo/WebClientConsumingTest.kt
@@ -1,14 +1,17 @@
 package com.example.demo
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class WebClientConsumingTest {
-
-	@Test
-	fun checkFetching(){
-		val client = WebClientConsuming()
-		client.webClientRestCall()
+@SpringBootTest(classes = [WebClientConsuming::class])
+class WebClientConsumingTest(@Autowired private val commentRepository: CommentRepository) {
+	@ParameterizedTest
+	@Throws(Exception::class)
+	fun saveCommentTest() {
+		val comment = Comment(123, 456, "Test Guy", "test@test.com", "TestTestTest")
+		commentRepository.save(comment)
+		Assertions.assertNotNull(comment.postId)
 	}
 }
