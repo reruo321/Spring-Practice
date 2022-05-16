11164f1163b27e29322daca1e72880b8a132c012
reruo <reruo321@gmail.com>
2022-05-17 05:42:00 +0900

    [Warning: Test is Not Working] updating test

--- Spring-Boot/Kotlin/007-Spring-Boot-with-MongoDB/src/test/kotlin/com/example/demo/WebClientConsumingTest.kt
+++ Spring-Boot/Kotlin/007-Spring-Boot-with-MongoDB/src/test/kotlin/com/example/demo/WebClientConsumingTest.kt
@@ -3,16 +3,13 @@ package com.example.demo
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.jupiter.params.ParameterizedTest
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.transaction.annotation.Transactional
 
-@ExtendWith(SpringExtension::class)
 @SpringBootTest
+@Transactional
 class WebClientConsumingTest(@Autowired private val commentRepository: CommentRepository) {
 	@Test
 	fun saveCommentTest() {
