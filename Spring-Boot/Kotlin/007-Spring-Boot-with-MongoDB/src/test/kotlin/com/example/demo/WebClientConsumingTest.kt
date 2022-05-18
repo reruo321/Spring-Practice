179fb9ad2b28a5e2634762c8d6857b77a76cd497
reruo <reruo321@gmail.com>
2022-05-19 01:49:51 +0900

    [Warning: Test is Not Working] updating test

--- Spring-Boot/Kotlin/007-Spring-Boot-with-MongoDB/src/test/kotlin/com/example/demo/WebClientConsumingTest.kt
+++ Spring-Boot/Kotlin/007-Spring-Boot-with-MongoDB/src/test/kotlin/com/example/demo/WebClientConsumingTest.kt
@@ -1,11 +1,14 @@
 package com.example.demo
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
-@Transactional
+@EnableAutoConfiguration(exclude=[MongoReactiveAutoConfiguration::class, MongoReactiveDataAutoConfiguration::class])
 class WebClientConsumingTest(){
 	@Test
 	fun loader(){
