f8fc715963082a9ffa5c90d5273aee1897afee76
reruo <reruo321@gmail.com>
2022-05-20 04:43:39 +0900

    [Warning: Test is Not Working] wow this one makes me crazy

--- Spring-Boot/Kotlin/007-Spring-Boot-with-MongoDB/src/test/kotlin/com/example/demo/WebClientConsumingTest.kt
+++ Spring-Boot/Kotlin/007-Spring-Boot-with-MongoDB/src/test/kotlin/com/example/demo/WebClientConsumingTest.kt
@@ -1,16 +1,18 @@
 package com.example.demo
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration
-import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.transaction.annotation.Transactional
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest
-@EnableAutoConfiguration(exclude=[MongoReactiveAutoConfiguration::class, MongoReactiveDataAutoConfiguration::class])
-class WebClientConsumingTest(){
-	@Test
-	fun loader(){
-	}
+
+@DataMongoTest //(properties = ["spring.mongodb.embedded.version=4.0.2"])
+@ExtendWith(SpringExtension::class)
+class WebClientConsumingTest{
+    @Test
+    fun commentSaveTest(@Autowired commentRepository: CommentRepository){
+        val comment = Comment(999, 12345, "Test Guy", "test@test.com", "Test!")
+        commentRepository.save(comment)
+    }
 }
