20a44051cad0cd19d2b6dbf8ebebda6a9d1de20d
reruo <reruo321@gmail.com>
2022-06-12 23:58:55 +0900

    committed

--- Spring-Boot/Kotlin/008-Spring-Boot-With-React-JS/src/main/kotlin/com/example/demo/DataRestApplication.kt
+++ Spring-Boot/Kotlin/008-Spring-Boot-With-React-JS/src/main/kotlin/com/example/demo/DataRestApplication.kt
@@ -1,4 +1,11 @@
 package com.example.demo
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
 class DataRestApplication {
+}
+fun main(){
+    SpringApplication.run(DataRestApplication::class.java)
 }
