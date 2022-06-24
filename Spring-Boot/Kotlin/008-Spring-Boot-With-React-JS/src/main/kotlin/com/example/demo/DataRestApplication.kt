e99bb8fe5a73c5e802c88ac0971c65945088c631
reruo <reruo321@gmail.com>
2022-06-24 21:13:08 +0900

    committed

--- Spring-Boot/Kotlin/008-Spring-Boot-With-React-JS/src/main/kotlin/com/example/demo/DataRestApplication.kt
+++ Spring-Boot/Kotlin/008-Spring-Boot-With-React-JS/src/main/kotlin/com/example/demo/DataRestApplication.kt
@@ -2,11 +2,9 @@ package com.example.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-    //(exclude=[DataSourceAutoConfiguration::class])
 @EnableJpaRepositories
 class DataRestApplication
 
