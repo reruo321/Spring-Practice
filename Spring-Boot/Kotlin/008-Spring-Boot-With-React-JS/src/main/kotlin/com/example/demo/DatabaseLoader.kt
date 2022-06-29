777f64e3107a7eaec3cf4bd3aeb3819492d71792
reruo <reruo321@gmail.com>
2022-06-29 19:32:00 +0900

    fixed, committed

--- Spring-Boot/Kotlin/008-Spring-Boot-With-React-JS/src/main/kotlin/com/example/demo/DatabaseLoader.kt
+++ Spring-Boot/Kotlin/008-Spring-Boot-With-React-JS/src/main/kotlin/com/example/demo/DatabaseLoader.kt
@@ -2,10 +2,12 @@ package com.example.demo
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 
 @Component
 class DatabaseLoader(@Autowired private val repository: BookRepository): CommandLineRunner {
+
     @Override
     @Throws(Exception::class)
     override fun run(vararg args: String?) {
